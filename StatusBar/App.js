758d51e15872c6da0447ae037e67cdc9ea1636b8
= <=>
2025-10-31 14:54:44 -0300

    Adicionando Alert e melhorando estilização.

--- StatusBar/App.js
+++ StatusBar/App.js
@@ -1,24 +1,52 @@
-import { StyleSheet, Text, View, StatusBar, Button } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  const alertaEsconder = () => {
+    Alert.alert(
+      'Alerta!',
+      'Você escondeu StatusBar!',
+    )
+  }
+  const alertaMostrar = () => {
+    Alert.alert(
+      'Alerta',
+      'Você mostrou StatusBar!',
+    )
+  }
+  const alerta3Botoes = () => {
+    Alert.alert(
+      'Alert com 3 opções', 'Deseja testar o botão com 3 botões?', [
+        {text : 'Sim', onPress : () => { Alert.alert('Alerta','Você escolheu Sim!')}},
+        {text : 'Não', onPress : () => { Alert.alert('Alerta','Você escolheu Não!')}},
+        {text : 'Perguntar depois', onPress : () => { Alert.alert('Alerta','Você escolheu Perguntar depois!')}}
+      ], {cancelable : false}
+    )
+  }
+
   return (
     <View style={styles.container}>
+      <Text style = {styles.txthead}>Olá! Esta é a página inicial. Seja bem-vindo(a)!</Text>
       <StatusBar barStyle={'light-content'}/>
       <Text style = {styles.text}>
-        Essa é a página inicial! Estou utilizando Statusbar.
+        Estou utilizando Statusbar.
       </Text>
-      <View style = {styles.meio}>
       <Button color ='#00f' title='Mostrar statusbar' onPress={() => {
         StatusBar.setHidden(false)
         StatusBar.setBarStyle('dark-content')
+        alertaMostrar()
       }}/>
-      </View>
-        
-      <View style = {styles.botoes}/>
+      <View style = {styles.meio}/>
       <Button color ='#044' title='Esconder statusbar' onPress={() => {
         StatusBar.setHidden(true)
         StatusBar.setBarStyle('dark-content')
+        alertaEsconder()
       }}/>
+
+      <View style = {styles.fim}>
+        <Text style = {styles.text}> Utilizando Alert.</Text>
+        <Button title="Alerta com 3 botões" onPress={ () => alerta3Botoes()} />
+      </View>
     </View>
   );
 }
@@ -30,13 +58,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  txthead : {
+    color : '#ef4',
+    textAlign : 'center',
+    fontFamily : 'monospace',
+    fontSize : 20,
+    fontStyle : 'italic',
+    fontWeight : 'bold',
+    padding : 30,
+  },
   text : {
     color : '#ecdd3cff',
     textAlign : 'center',
     fontFamily : 'monospace',
     fontSize : 20,
+    padding : 15,
   },
   meio : {
     padding : 10,
+  },
+  fim : {
+    padding : 40,
   }
 });
